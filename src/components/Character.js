766d882cc58cc1d524983c908f93c832cee4f95b
Alexander Treimanis <treimanis@Alexanders-Air.cable.rcn.com>
2022-04-22 15:43:57 -0500

    rendering characters to the dom

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = props => {
+    return (
+        <div className='character'>
+        {props.info.name}
+
+    </div>
+)
+}
+
+export default Character;
